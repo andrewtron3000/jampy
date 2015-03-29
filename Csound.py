fb2b6d2b77a992a5c71ee52e524ab17b4db172a5
Andrew <andrew.unit@gmail.com>
2015-03-28 23:35:46 -0400

    initial addition

---
+++ Csound.py
@@ -0,0 +1,150 @@
+"""
+Csound.py - A set of Python classes for Csound instruments and scores
+using the CSD Csound input file format.
+"""
+
+import string, types, os, sys, math
+from random import uniform
+from pitch_table import pitch_table
+
+AMPLITUDE_MAXIMUM = math.pow(2, 15) - 1
+
+def tabjoin(*args):
+    return string.join(map(str, args), '\t')
+
+def if_none(test_value, result_value):
+    if test_value == None:
+        return result_value
+    else:
+        return test_value
+
+def parse_pitch(pitch):
+    if type(pitch) == types.StringType:
+        offset = 0
+        if '+' in pitch:
+            pitch, offset = pitch.split('+')
+            offset = float(offset)
+        hz = pitch_table.get(pitch)
+        if not hz:
+            print 'Error: Unknown pitch name: "%s"' % (pitch)
+            sys.exit(1)
+        return hz + offset
+    else:
+        return pitch
+
+def convert_amplitude(amplitude):
+    return min(amplitude * AMPLITUDE_MAXIMUM, AMPLITUDE_MAXIMUM)
+
+# Parent instrument class:
+
+class Instrument:
+    serial_number = 1
+    def __init__(self):#, number):
+        #self.number = number
+        self.number = Instrument.serial_number
+        Instrument.serial_number += 1
+
+    def orchestra(self):#, number):
+        output, opcodes = self.opcodes()
+        result = '\tinstr\t%d\n' % (self.number)
+        result += opcodes + '\n'
+        result += '\tout\t%s\n\tendin\n' % (output)
+        return result
+
+# Individual instruments:
+
+class oscil(Instrument):
+    def opcodes(self):
+        return 'a1*k1', 'k1\tlinseg\t0, .012, 1, p3 - 0.036, 1, .024, 0\na1\toscil\tp4, p5, 1'
+
+    def note(self, start, duration, frequency, amplitude=.5):
+        return tabjoin('i', self.number, start, duration,
+                       convert_amplitude(amplitude), parse_pitch(frequency))
+
+class buzz(Instrument):
+    def __init__(self, number_of_harmonics=10):
+        Instrument.__init__(self)
+        self.number_of_harmonics = number_of_harmonics
+
+    def opcodes(self):
+        return 'a1', 'a1\tbuzz\tp4, p5, p6, 1'
+    
+    def note(self, start, duration, frequency, amplitude=.5,
+             number_of_harmonics=None):
+        return tabjoin('i', self.number, start, duration,
+                       convert_amplitude(amplitude), parse_pitch(frequency),
+                       if_none(number_of_harmonics, self.number_of_harmonics))
+
+class fmvoice(Instrument):
+    def __init__(self, vowel=0, spectral_tilt=0, vibrato_depth=0.005, vibrato_rate=6):
+        Instrument.__init__(self)
+        self.vowel = vowel
+        self.spectral_tilt = spectral_tilt
+        self.vibrato_depth = vibrato_depth
+        self.vibrato_rate = vibrato_rate
+
+    def opcodes(self):
+        return 'a1', 'a1\tfmvoice\tp4, p5, p6, p7, p8, p9, 1, 1, 1, 1, 1'
+
+    def note(self, start, duration, frequency, amplitude = .5,
+             vowel=None, spectral_tilt=None, vibrato_depth=None, vibrato_rate=None):
+        return tabjoin('i', self.number, start, duration,
+                       convert_amplitude(amplitude), parse_pitch(frequency),
+                       if_none(vowel, self.vowel),
+                       if_none(spectral_tilt, self.spectral_tilt),
+                       if_none(vibrato_depth, self.vibrato_depth),
+                       if_none(vibrato_rate, self.vibrato_rate))
+
+
+# CSD organization and output:
+
+class CSD:
+    def __init__(self, output_csd_filename='default.csd', render_sound=0):
+        self.set_filenames(output_csd_filename)
+        self.render_sound = render_sound
+        self.options = '-A -o %s' % (self.output_sound_filename)
+        self.instruments = []
+        self.note_list = ''
+        print 'Creating CSD file "%s"' % (self.output_csd_filename)
+        if render_sound:
+            print '; rendering to file "%s"' % (self.output_sound_filename)
+
+    def set_filenames(self, output_csd_filename):
+        self.output_csd_filename = output_csd_filename
+        if output_csd_filename.endswith('.csd'):
+            self.output_sound_filename = output_csd_filename[:-4] + '.aif'
+        else:
+            self.output_sound_filename = output_csd_filename + '.aif'
+
+    def orchestra(self, *args):
+        self.instruments += args
+
+    def orchestra_definition(self):
+        result = ''
+        for inst in self.instruments:
+            result += inst.orchestra() + '\n'
+        return string.rstrip(result)
+
+    def tables(self):
+        return tabjoin('f', 1, 0, 4096, 10, 1)
+
+    def score(self, *args):
+        self.note_list += string.join(args, '\n') + '\n'
+
+    def score_definition(self):
+        return string.rstrip(self.tables() + '\n' + self.note_list)
+
+    def tagify(self, tag_name, s, newlines=1):
+        return '\n<%s>\n%s\n</%s>\n' % (tag_name, s, tag_name)
+
+    def output(self, output_csd_filename = None):
+        if output_csd_filename:
+            self.set_filenames(output_csd_filename)
+        fp = open(self.output_csd_filename, 'w')
+        fp.write(self.tagify('CsoundSynthesizer',
+                             self.tagify('CsOptions', self.options) +
+                             self.tagify('CsInstruments', self.orchestra_definition()) +
+                             self.tagify('CsScore', self.score_definition()), 0))
+        fp.close()
+        if self.render_sound:
+            os.system('/Users/ack/csound %s' % (self.output_csd_filename))
---
+++ generator.py
@@ -0,0 +1,153 @@
+import random
+import time
+import sys
+import Csound
+import subprocess
+import base64
+import hashlib
+
+csd = None
+oscillator = None
+buzzer = None
+voice = None
+song_publisher = None
+
+def add_motif(instrument, req):
+    global csd
+    note_bank = req.note_bank.split()
+    notes = random.sample(note_bank, len(note_bank))
+    playit = [ random.choice([True, False]) for n in notes]
+    chorus = zip(notes, playit)
+    time = req.motif_start_time
+    repeat = req.motif_repeat
+    for (note, playit) in (chorus * repeat):
+        if playit:
+            csd.score(instrument.note(time, 
+                                      req.note_duration, 
+                                      note, 
+                                      req.motif_amplitude))
+        time += req.note_duration + req.internote_delay
+
+def handle_add_oscil_motif(req):
+    global oscillator
+    return add_motif(oscillator, req)
+
+def handle_add_buzzer_motif(req):
+    global buzzer
+    return add_motif(buzzer, req)
+
+def handle_create_song(req):
+    global csd, oscillator, buzzer, voice
+    global song_publisher
+    s = 'temp'
+    csd = Csound.CSD('%s.csd' % s) 
+    csd.orchestra(oscillator, buzzer, voice) 
+    for motif in req.motifs:
+        if motif.instrument == 'oscil':
+            add_motif(oscillator, motif)
+        elif motif.instrument == 'buzzer':
+            add_motif(buzzer, motif)
+        elif motif.instrument == 'voice':
+            add_motif(voice, motif)
+    csd.output()
+    args = ['csound', '-d', '%s.csd' % s] 
+    subprocess.call(args)
+    f = open('%s.csd' % s)
+    csd_string = f.read()
+    f.close()
+    song_name = '%s.ogg' % req.song_name
+    args = ['oggenc', '-o', song_name, '%s.aif' % s]
+    subprocess.call(args)
+    args = ['vorbiscomment', '-a', song_name,  
+            '-t', "ARTIST=%s" % req.artist,
+            '-t', "TITLE=%s" % req.song_name,
+            '-t', "ALBUM=%s" % req.album,
+            '-t', "GENRE=%s" % 'Electronica',
+            '-t', "CSOUND=%s" % csd_string]
+    subprocess.call(args)
+    args = ['ogg123', song_name]
+    subprocess.call(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Motif(object):
+    def __init__(self, motif_start_time, motif_repeat, motif_amplitude, note_bank, note_duration, internote_delay, instrument):
+        self.motif_start_time = motif_start_time
+        self.motif_repeat = motif_repeat
+        self.motif_amplitude = motif_amplitude
+        self.note_bank = note_bank
+        self.note_duration = note_duration
+        self.internote_delay = internote_delay
+        self.instrument = instrument
+
+
+
+
+
+class Request(object):
+    def __init__(self, song_name, artist, album, motifs):
+        self.song_name = song_name
+        self.artist = artist
+        self.album = album
+        self.motifs = motifs    
+
+def heads():
+    return (random.random() < 0.5)
+
+def biasedFlip(p):
+    return (random.random() < p)
+
+def selectInstrument():
+    if heads():
+        return 'oscil'
+    else:
+        return 'buzzer'
+
+def selectInterval():
+    return 0.15, 0.05
+
+def triggerCreate(song_name, artist, album, motifs):
+    handle_create_song(Request(song_name, artist, album, motifs))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Usage: %s <artist> <album name>" % sys.argv[0]
+        exit()
+    else:
+        artist = sys.argv[1]
+        album = sys.argv[2]
+    global song_publisher, oscillator, buzzer, voice
+    oscillator = Csound.oscil() 
+    buzzer = Csound.buzz() 
+    voice = Csound.fmvoice() 
+    for i in xrange(1, 16384):
+        song_title = "song_%d" % i
+        if heads():
+            a = 0.05
+            b = 0.15
+        else:
+            a = 0.15
+            b = 0.05
+        motifs = [ Motif(0.0, 12, 0.32, "A3 B3 D4 E4 F#4 A4 B4 D5 E5 F#5 A5 B5 D6 E6 F#6", 0.15, 0.05, selectInstrument()) ]
+        if biasedFlip(0.8):
+            motifs.append(Motif(3.0, 10, 0.32, "A3 B3 D4 E4 F#4 A4 B4 D5 E5 F#5 A5 B5 D6 E6 F#6",    a,    b, selectInstrument()))
+        if biasedFlip(0.9):
+            motifs.append(Motif(6.0,  4, 0.10, "A2 B2 D3 D3 F#3 A3 B3 D4 E4 F#4 A4 B4 D5 E5 F#5",  0.3,  0.1, selectInstrument()))
+        triggerCreate(song_title, artist, album, motifs)
+        print "Created song %s" % song_title
+        time.sleep(10)
+
